40372c6fa348ba188035feec312ef215ecd88b4c
006ZERO <gaithslman7@gmail.com>
2026-08-26 15:26:12 +0300

    editing memove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -1,38 +1,19 @@
 #include "libft.h"
 
-static void	move_forward(unsigned char *d, const unsigned char *s, size_t n)
+void    *ft_memmove(void *dst, const void *src, size_t len)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
-}
-
-static void	move_backward(unsigned char *d, const unsigned char *s, size_t n)
-{
-	while (n > 0)
-	{
-		n--;
-		d[n] = s[n];
-	}
-}
-
-void	*ft_memmove(void *dst, const void *src, size_t n)
-{
-	unsigned char		*d;
-	const unsigned char	*s;
-
-	d = (unsigned char *)dst;
-	s = (const unsigned char *)src;
-	if (d == NULL && s == NULL)
-		return (NULL);
-	if (d < s)
-		move_forward(d, s, n);
-	else
-		move_backward(d, s, n);
-	return (dst);
+    unsigned char *d;
+    unsigned char *s;
+    
+    d = (unsigned char *)dst;
+    s = (unsigned char *)src;
+    if (!dst && !src)
+        return (dst);
+    if (src < dst)
+        while (len--)
+            d[len] = s[len];
+    else 
+        while (len--)
+            *d++ = *s++;
+    return (dst);
 }
